cd16679be0a70705aa6daec49b0f66519b9a1a18
Gabriel Barimboim <gdbarimboim@gmail.com>
2022-10-02 16:38:24 -0300

    ready

---
+++ index.js
@@ -0,0 +1,11 @@
+const container = document.querySelectorAll(".container");
+
+let aux = "";
+container.forEach((call) => {
+  call.addEventListener("click", (change) => {
+    call.classList.add("open");
+    call.addEventListener("mouseleave", (change) => {
+      call.classList.remove("open");
+    });
+  });
+});
